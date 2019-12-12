6d9e82e97ddc6f71930120b90996ee1c9ae1ddbe
JacoDTDev <jacodtdev@gmail.com>
2019-12-12 15:43:33 +0200

    Insert into db via a form, preventing a full screen refers using e.preventDefault();

--- client/main.js
+++ client/main.js
@@ -10,6 +10,19 @@ const renderPlayers = function (playersList) {
     });
 };
 
+const handleSubmit = function(e){
+    e.preventDefault();
+    let playerName = e.target.playerName.value;
+
+    if(playerName){
+        e.target.playerName.value = '';
+        Players.insert({
+            name: playerName,
+            score: 0
+        })
+    }
+};
+
 Meteor.startup(function () {
     Tracker.autorun(()=>{
         const players = Players.find().fetch()
@@ -21,6 +34,10 @@ Meteor.startup(function () {
                 <p>Hello {name}!</p>
                 <p>This is my second p.</p>
                 {renderPlayers(players)}
+                <form onSubmit={handleSubmit}>
+                    <input type='text' name='playerName' placeholder='Player name'/>
+                    <button>Add Player</button>
+                </form>
             </div>
         );
         ReactDom.render(jsx,document.getElementById('app'))
