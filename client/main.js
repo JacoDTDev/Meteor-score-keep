eff12fb5ac6b071e62940cfc33bc57a664b453c2
JacoDTDev <jacodtdev@gmail.com>
2019-12-12 11:19:16 +0200

    showing how to render a array in jsx, and how to call a function

--- client/main.js
+++ client/main.js
@@ -2,11 +2,17 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const renderPlayer=()=>{
+    return [<p key='1'>1</p>,<p key='2'>2</p> ]
+};
 Meteor.startup(() => {
   // code to run on server at startup
+    const title = 'Score Keep';
+
     const jsx = (
         <div>
-            <h1>Score Keep</h1>
+            <h1>{title}</h1>
+            {renderPlayer()}
         </div>
     );
     ReactDom.render(jsx,document.getElementById('app'))
