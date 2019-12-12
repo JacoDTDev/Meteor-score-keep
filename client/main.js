5812e2cee17ec59043d28a4eccd3566589985bf4
JacoDTDev <jacodtdev@gmail.com>
2019-12-12 16:27:51 +0200

    Updating Players Document

--- client/main.js
+++ client/main.js
@@ -6,7 +6,11 @@ import {Players} from "../imports/api/players";
 
 const renderPlayers = function (playersList) {
     return playersList.map(function (player) {
-        return <p key={player._id}>{player.name} has {player.score} point(s).<button onClick={()=> Players.remove({_id:player._id})}>remove</button></p>;
+        return <p key={player._id}>{player.name} has {player.score} point(s).
+            <button onClick={()=> Players.update({_id:player._id},{$inc:{score:1}})}>+1</button>
+            <button onClick={()=> Players.update({_id:player._id},{$inc:{score:-1}})}>-1</button>
+            <button onClick={()=> Players.remove({_id:player._id})}>X</button>
+        </p>;
     });
 };
 
