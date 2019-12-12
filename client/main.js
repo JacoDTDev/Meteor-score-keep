3eac2831537b56f682f2089074d093e498f604ba
JacoDTDev <jacodtdev@gmail.com>
2019-12-12 11:04:55 +0200

    Changed the default app to work with react.

--- client/main.js
+++ client/main.js
@@ -1,22 +1,13 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  // code to run on server at startup
+    const jsx = (
+        <div>
+            <h1>Score Keep</h1>
+        </div>
+    );
+    ReactDom.render(jsx,document.getElementById('app'))
 });
