65cad33035305ea7aa25c0b70ea9b110a72df28e
JacoDTDev <jacodtdev@gmail.com>
2019-12-12 11:48:12 +0200

    Using the .map function

--- client/main.js
+++ client/main.js
@@ -3,8 +3,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const renderPlayer=()=>{
+    let numbers =[{name:1,val:1},{name:2,val:2},{name:3,val:3}];
+
+    let newNumber=numbers.map((number)=>{
+        return number.val
+    });
+    console.log(newNumber);
+
     return [<p key='1'>1</p>,<p key='2'>2</p> ]
 };
+
 Meteor.startup(() => {
   // code to run on server at startup
     const title = 'Score Keep';
